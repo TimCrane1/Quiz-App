51110589c94ccb9ace50a654edd07bd99bd31267
timothy crane <timothycrane@timothys-MacBook-Pro.local>
2016-02-29 22:52:39 -0500

    almost functional

--- beats.js
+++ beats.js
@@ -1,26 +1,35 @@
-/*$(document).ready(function() {
-$("#quest").on('click', function() {
-  $('#rules').text('\"Because the world is round it turns me on\"...Beatles or Beat?');
+
+$(document).ready(function(){
+	$('#quest').on('click', function() {
+		var i =0;
+		for(i = 0; i < listQuest.length; i++) {
+			alert(i);
+			$('#rules').text(listQuest[i].query + '...Beatles or Beat?');
+	$('.choice').on('click', function() {
+		$('#rules').text('Correct! ' + listQuest[i]);
 });
-});*/
-
-
-function Question(query, answer, explanation) {
-this.query = query;
-this.answer = answer;
 }
-var question1 = new Question('Because the world is round it turns me on', 'Beatles', 'Line #1 if from the Beatles song, "Because".');
-var question2 = new Question('I\'m with you in Rockland, where you must feel very strange', 'Beat', 'Line #2 is from Allen Ginsgerg\'s poem, "Howl".');
-var question3 = new Question("Yellow matter custard, Dripping from a dead dog\'s eye", 'Beatles', 'Line #3 is from the Beatles song, "I Am the Walrus".');
-var question4 = new Question('You were not there for the beginning. You will not be there for the end.', 'Beat', 'Line #4 is from the William S. Burroughs novel, "Naked Lunch".');
-var question5 = new Question("Let\'s all get up and dance to a song that was a hit before your mother was born", 'Beatles', 'Line #5 is from the Beatles song, "Your Mother Should Know".');
+});
+ 
+function question(query, answer, explanation) {
+	this.query = query;
+	this.answer = answer;
+	this.explanation = explanation;
+}
+var question1 = new question('Because the world is round it turns me on', 'Beatles', 'Line #1 is if from the Beatles song, \"Because\".');
+var question2 = new question('I\'m with you in Rockland, where you must feel very strange', 'Beat', 'Line #2 is from Allen Ginsgerg\'s poem, "Howl".');
+var question3 = new question("Yellow matter custard, Dripping from a dead dog\'s eye", 'Beatles', 'Line #3 is from the Beatles song, "I Am the Walrus".');
+var question4 = new question('You were not there for the beginning. You will not be there for the end.', 'Beat', 'Line #4 is from the William S. Burroughs novel, "Naked Lunch".');
+var question5 = new question("Let\'s all get up and dance to a song that was a hit before your mother was born", 'Beatles', 'Line #5 is from the Beatles song, "Your Mother Should Know".');
 var listQuest = [question1, question2, question3, question4, question5];
 var questionsCorrect = [];
 var questionsAnswered = [];
-var rightAnswer = "Correct! " + listQuest[i].explanation + "Click on " + #quest + " to continue.";
-var wrongAnswer = "That's not the right answer. " + listQuest[i].explanation + "Click on " #quest + " to continue."; 
 var footer = questionsCorrect + " out of " + questionsAnswered + " correct";
-var #quest = listQuest[0];
+
+});
+
+
+
 /*this is what I want to do in pseudo-code
 onclick #quest <button>,
 #rules <p> gets replaced by  question1.query,
@@ -29,4 +38,3 @@ if (.choice == .query) {push 1 to questionsCorrect and Answered arrays, return r
 else {push 1 to questionsAnswered, return wrongAnswer and footer}
 while(#quest < listQuest.length){#quest = listQuest position + 1}-- i want #quest to be a variable in the html, not text?
 */
- 
---
+++ beats2.js
@@ -0,0 +1,41 @@
+
+$(document).ready(function(){
+	var arrPosition = 0;
+	var questionsCorrect = 0;
+    var questionsAnswered = 0;
+	$('#quest').on('click', function() {
+		$('#rules').text(listQuest[arrPosition].query + '...Beatles or Beat?');
+	});
+	$('.choice').on('click', function() {
+		if ($(this).attr("value") == listQuest[arrPosition].answer) {
+		$('#rules').text('Correct! ' + listQuest[arrPosition].explanation);
+		questionsCorrect += 1;
+	}
+		else {
+			$('#rules').text('That wasn\'t right. ' + listQuest[arrPosition].explanation);
+		}
+		arrPosition += 1;
+		questionsAnswered += 1;
+		if (arrPosition < listQuest.length) {
+			$('#quest').text('question #' + (arrPosition + 1));
+		}
+		$('#tally').text(footer);
+	});
+  
+function question(query, answer, explanation) {
+	this.query = query;
+	this.answer = answer;
+	this.explanation = explanation;
+}
+var question1 = new question('Because the world is round it turns me on', 'Beatles', 'Line #1 is if from the Beatles song, \"Because\". Click on the next question.');
+var question2 = new question('I\'m with you in Rockland, where you must feel very strange', 'Beat', 'Line #2 is from Allen Ginsgerg\'s poem, "Howl". Click on the next question.');
+var question3 = new question("Yellow matter custard, Dripping from a dead dog\'s eye", 'Beatles', 'Line #3 is from the Beatles song, "I Am the Walrus". Click on the next question.');
+var question4 = new question('You were not there for the beginning. You will not be there for the end.', 'Beat', 'Line #4 is from the William S. Burroughs novel, "Naked Lunch". Click on the next question.');
+var question5 = new question("Let\'s all get up and dance to a song that was a hit before your mother was born", 'Beatles', 'Line #5 is from the Beatles song, "Your Mother Should Know". Good game!');
+var listQuest = [question1, question2, question3, question4, question5];
+var footer = questionsCorrect + " out of " + questionsAnswered + " correct";
+});
+
+
+
+
